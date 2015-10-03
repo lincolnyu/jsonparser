c260b10f38a3f7967782baec2662c4a31e32c490
Lincoln Yu <quanben@msn.com>
2015-10-03 23:20:58 +1000

    added JsonParisHelper.

---
+++ JsonParser/JsonParser/Helpers/JsonPairsHelper.cs
@@ -0,0 +1,29 @@
+﻿using JsonParser.JsonStructures;
+
+namespace JsonParser.Helpers
+{
+    public static class JsonPairsHelper
+    {
+        public static JsonNode GetNodeOrNull(this JsonPairs jsPairs, string key)
+        {
+            JsonNode node;
+            if (jsPairs.KeyValues.TryGetValue(key, out node))
+            {
+                return node;
+            }
+            return null;
+        }
+
+        public static T GetNodeOrNull<T>(this JsonPairs jsPairs, string key) where T : JsonNode
+        {
+            var node = jsPairs.GetNodeOrNull(key);
+            return node as T;
+        }
+
+        public static T GetValueOrDefault<T>(this JsonPairs jsPairs, string key)
+        {
+            var jsNode = jsPairs.GetNodeOrNull<JsonValue<T>>(key);
+            return jsNode != null ? jsNode.Value : default(T);
+        }
+    }
+}
--- JsonParser/JsonParser/JsonStructures/JsonPairs.cs
+++ JsonParser/JsonParser/JsonStructures/JsonPairs.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace JsonParser.JsonStructures
@@ -47,7 +48,6 @@ namespace JsonParser.JsonStructures
             }
             return sb.ToString();
         }
-
 
         /// <summary>
         ///  
